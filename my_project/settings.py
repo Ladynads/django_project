3e0e5f6f171a1a76abdf2bd5e59ee7572ea85f7a
Miss Nadia <gbqlaced@gmail.com>
2025-03-03 21:33:15 +0000

    feat: add Procfile and update settings for production deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fwc6(!fpuhcfla(*t37whm#k2wvst*_nimr1j662nkj5^ufke%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
