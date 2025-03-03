57d8387103587f7e24e72e1050d275ec937fb5e0
Miss Nadia <gbqlaced@gmail.com>
2025-03-03 21:21:21 +0000

    feat: add post_check view and corresponding URL path

--- hello_world/views.py
+++ hello_world/views.py
@@ -8,8 +8,8 @@ def index(request):
 def about_me(request):
     return HttpResponse("This would be the about page")
 
-if request.method == "POST":
+def post_check(request):
+    if request.method == "POST":
         return HttpResponse("You must have POSTed something")
     else:
         return HttpResponse(request.method)
-
--- my_project/urls.py
+++ my_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('hello/', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('post-check/', index_views.post_check, name='post_check'),
     path('', index_views.index, name='home'),
     
     
